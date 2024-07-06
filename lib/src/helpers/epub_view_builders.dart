82c541c54e623c19da79362bb48316ca5055f65c
HenriqueRib <ribeiro.henriquem@gmail.com>
2024-07-06 16:41:07 -0300

    Modificação com ajuda do luis

--- lib/src/helpers/epub_view_builders.dart
+++ lib/src/helpers/epub_view_builders.dart
@@ -29,6 +29,7 @@ typedef ChaptersBuilder = Widget Function(
   ExternalLinkPressed onExternalLinkPressed,
   OnSelectedChanged onSelectedChanged,
   OnTextToSpeech onTextToSpeech,
+  OnHighlight onHighlight,
 );
 
 typedef ChapterDividerBuilder = Widget Function(EpubChapter value);
