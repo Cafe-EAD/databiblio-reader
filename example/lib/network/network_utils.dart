f0bd773c1cdde2168a2e9016568d82da1b12f6b7
HenriqueRib <ribeiro.henriquem@gmail.com>
2024-07-10 09:41:29 -0300

    Adicionando o headers de volta e a url do databiblio-reader no pubspec.yaml

--- example/lib/network/network_utils.dart
+++ example/lib/network/network_utils.dart
@@ -56,15 +56,15 @@ Future<Response> buildHttpResponse(String endPoint,
       response = await http.post(
         url,
         body: jsonEncode(request),
-        // headers: headers,
+        headers: headers,
         encoding: isStripePayment ? Encoding.getByName("utf-8") : null,
       );
     } else if (method == HttpMethod.DELETE) {
-      response = await delete(url);
+      response = await delete(url, headers: headers);
     } else if (method == HttpMethod.PUT) {
       response = await put(url, body: jsonEncode(request));
     } else {
-      response = await get(url);
+      response = await get(url, headers: headers);
     }
 
     log('Response (${method.name}) ${response.statusCode}: ${response.body}');
