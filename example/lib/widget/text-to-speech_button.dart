8af55305c2d8266aaea4bfcad835e247c85c3cc7
davidwajch <dwajchenberg@gmail.com>
2024-07-16 10:33:55 -0300

    menu tts

--- example/lib/widget/text-to-speech_button.dart
+++ example/lib/widget/text-to-speech_button.dart
@@ -3,13 +3,11 @@ import 'package:flutter_tts/flutter_tts.dart';
 
 class TextToSpeechButton extends StatefulWidget {
   final String texto;
-  const TextToSpeechButton(this.texto, {Key? key})
-      : super(key: key);
+  const TextToSpeechButton(this.texto, {Key? key}) : super(key: key);
 
   @override
   TextToSpeechButtonState createState() => TextToSpeechButtonState();
 }
-
 
 class TextToSpeechButtonState extends State<TextToSpeechButton>
     with TickerProviderStateMixin {
@@ -17,11 +15,11 @@ class TextToSpeechButtonState extends State<TextToSpeechButton>
   bool isPlaying = false;
   final int maxCar = 3900;
   final FlutterTts _flutterTts = FlutterTts();
-  List<Map> _voices = [];
-  Map? _currentVoice;
+  List<Map<String, String>> _voices = [];
+  Map<String, String>? _currentVoice;
 
- int chunkAtual=0;
-List<String>? textChunks;
+  int chunkAtual = 0;
+  List<String>? textChunks;
   @override
   void initState() {
     super.initState();
@@ -31,13 +29,13 @@ List<String>? textChunks;
     );
     initTTS();
     loadText();
+    _loadVoices();
   }
 
-void loadText()  {
-  var texto = widget.texto.replaceAll('\n', '');
- textChunks =  _splitTextIntoChunks(texto, maxCar);
-  
-}
+  void loadText() {
+    var texto = widget.texto.replaceAll('\n', '');
+    textChunks = _splitTextIntoChunks(texto, maxCar);
+  }
 
   @override
   void dispose() {
@@ -49,69 +47,161 @@ void loadText()  {
   void _handleOnPressed() async {
     setState(() {
       if (isPlaying) {
-       _flutterTts.stop();
+        _flutterTts.stop();
         _controller.reverse();
       } else {
-        chunkAtual=0;
+        chunkAtual = 0;
         _flutterTts.speak(textChunks![chunkAtual]);
         _controller.forward();
-            }
+      }
       isPlaying = !isPlaying;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: AnimatedIcon(
-        icon: AnimatedIcons.play_pause,
-        progress: _controller,
+    return GestureDetector(
+      onLongPress: () {
+        final RenderBox button = context.findRenderObject() as RenderBox;
+        final RenderBox overlay =
+            Overlay.of(context).context.findRenderObject() as RenderBox;
+        final RelativeRect position = RelativeRect.fromRect(
+          Rect.fromPoints(
+            button.localToGlobal(Offset.zero, ancestor: overlay),
+            button.localToGlobal(button.size.bottomRight(Offset.zero),
+                ancestor: overlay),
+          ),
+          Offset.zero & overlay.size,
+        );
+        showMenu(
+          context: context,
+          position: position,
+          items: <PopupMenuEntry<String>>[
+            const PopupMenuItem<String>(
+              value: 'language',
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Voz'), Icon(Icons.language)],
+              ),
+            ),
+            PopupMenuItem<String>(
+              value: 'stop',
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(isPlaying ? 'Parar' : 'Play'),
+                  Icon(isPlaying ? Icons.stop : Icons.play_arrow)
+                ],
+              ),
+            ),
+          ],
+        ).then((value) {
+          if (value != null) {
+            switch (value) {
+              case 'stop':
+                _handleOnPressed();
+                break;
+              case 'language':
+                _showVoiceSelector();
+                break;
+            }
+          }
+        });
+      },
+      child: IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.play_pause,
+          progress: _controller,
+        ),
+        onPressed: _handleOnPressed,
       ),
-      onPressed: _handleOnPressed,
     );
   }
 
   void initTTS() {
-    // posiçao no texto 
+    // posiçao no texto
     // _flutterTts.setProgressHandler((text, start, end, word) {
     //   setState(() {
     //     _currentWordStart = start;
     //     _currentWordEnd = end;
     //   });
     // });
-    _flutterTts.getVoices.then((data) {
-      try {
-        List<Map> voices = List<Map>.from(data);
-        setState(() {
-          _voices =
-              voices.where((voice) => voice["name"].contains("en")).toList();
-          _currentVoice = _voices.first;
-          _setVoice(_currentVoice!);
-        });
-      } 
-      catch (e) {
-        SnackBar(content: Text('$e'),);
-      }
-    });
     _flutterTts.setCompletionHandler(() {
       setState(() {
-        if(textChunks!.length==chunkAtual){
-        // print("Complete");
-        _controller.reverse();
-        isPlaying=false;
-        }else{
+        if (textChunks!.length == chunkAtual) {
+          // print("Complete");
+          _controller.reverse();
+          isPlaying = false;
+        } else {
           chunkAtual++;
           _flutterTts.speak(textChunks![chunkAtual]);
         }
       });
     });
-
   }
 
-    void _setVoice(Map voice) {
+  void _setVoice(Map voice) {
     _flutterTts.setVoice({"name": voice["name"], "locale": voice["locale"]});
   }
 
+  void _showVoiceSelector() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Selecione uma voz'),
+          content: DropdownButton<Map<String, String>>(
+            value: _currentVoice,
+            items: _voices
+                .map(
+                  (voice) => DropdownMenuItem<Map<String, String>>(
+                    value: voice,
+                    child: Text(
+                      voice["name"]!,
+                    ),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                _currentVoice = value;
+              });
+              if (value != null) {
+                _setVoice(value);
+              }
+              Navigator.of(context).pop();
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _loadVoices() async {
+    var data = await _flutterTts.getVoices;
+    try {
+      _voices = (data as List)
+          .map((voice) => Map<String, String>.from(voice))
+          .toList();
+      setState(() {
+        if (_voices.isNotEmpty) {
+          _currentVoice =
+              _voices.firstWhere((voice) => voice["name"]!.contains("en"));
+          _setVoice(_currentVoice!);
+        }
+      });
+    } catch (e) {
+
+    }
+  }
 }
 
 List<String> _splitTextIntoChunks(String text, int maxSize) {
@@ -135,7 +225,6 @@ List<String> _splitTextIntoChunks(String text, int maxSize) {
     chunks.add(text.substring(start, lastSpace).trim());
     start = lastSpace + 1;
   }
-
 
   return chunks;
 }
